35e502a1b3661431678ce25b8fe19b487a5df640
Evelyn Gonzalez <evieglz369@gmail.com>
2018-08-10 20:30:00 -0500

    updated project

--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 
   var animals = [
-    "dog", "cat", "rabbit", "hamster", "skunk", "goldfish",
-    "bird", "ferret", "turtle", "sugar glider", "chinchilla",
-    "hedgehog", "hermit crab", "gerbil", "pygmy goat", "chicken",
-    "capybara", "teacup pig", "serval", "salamander", "frog"
+    "dog", "cat", "rabbit", "hamster", "skunk", "beta fish",
+    "bird", "fox", "ferret", "turtle", "sugar glider", "otter",
+    "hedgehog", "gerbil", "emperor penguin",
+     "serval", "salamander", "blue footed boobies", "raccoon"
   ];
 
   // function to make buttons and add to page
